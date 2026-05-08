fe529bab09de7a476594a14df83a8e7c9d138bf0
(cyberpolin) = 'Carlos González <cyberpolin@gmail.com>
2026-05-07 21:00:07 -0600

    web changes to redirect

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/papas-gratis",
+        destination: "https://papasgratis.lamojarreria.com",
+        permanent: true, // Returns a 308 status code
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
